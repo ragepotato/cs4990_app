8064af9797a386f0b308a7bb55a9e4c50bf633ef
Stephen Tayag <stephen.taya@gmail.com>
2020-02-16 23:15:40 -0800

    Assignment 2. Added background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:ui';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -29,142 +29,162 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
       backgroundColor: Colors.blue,
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 30,
-              child: Container(
-                //margin: EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  //crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(top: 40),
-                      child: Text(
-                        "SeeNext",
-                        style: TextStyle(color: Colors.white, fontSize: 50.0),
-                      ),
-                    ),
+      // body: new Stack(
 
-                    //Text("Hello"),
-                  ],
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("Parasite-poster-2.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: new BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0,),
+              child: new Container(
+                decoration: new BoxDecoration(
+                  color: Colors.black.withOpacity(0.2),
                 ),
               ),
             ),
-            Expanded(
-              flex: 40,
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    FlatButton(
-                      color: Colors.black,
-                      textColor: Colors.white,
-                      disabledColor: Colors.grey,
-                      disabledTextColor: Colors.black,
-                      //padding: EdgeInsets.all(16.0),
+          ),
 
-                      padding: EdgeInsets.only(
-                          left: 54.0, top: 25.0, right: 54.0, bottom: 25.0),
-                      splashColor: Colors.blueAccent,
-                      onPressed: () {
-                        print("New Search activated.");
-                      },
-                      child: Text(
-                        "NEW SEARCH",
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ),
-                    FlatButton(
-                      color: Colors.black,
-                      textColor: Colors.white,
-                      disabledColor: Colors.grey,
-                      disabledTextColor: Colors.black,
-                      padding: EdgeInsets.only(
-                          left: 50.0, top: 15.0, right: 50.0, bottom: 15.0),
-                      splashColor: Colors.blueAccent,
-                      onPressed: () {
-                        print("Search by preferences activated.");
-                      },
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            "SEARCH BY",
-                            style: TextStyle(fontSize: 20.0),
+          Center(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 30,
+                  child: Container(
+                    //margin: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(top: 40),
+                          child: Text(
+                            "SeeNext",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 50.0),
                           ),
-                          Text(
-                            "PREFERENCES",
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                        ],
-                      ),
+                        ),
+
+                        //Text("Hello"),
+                      ],
                     ),
-                    //Text("Hello"),
-                    //Text("Hello"),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 30,
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    FlatButton(
-                      color: Colors.black,
-                      textColor: Colors.white,
-                      disabledColor: Colors.grey,
-                      disabledTextColor: Colors.black,
-                      //padding: EdgeInsets.all(16.0),
+                Expanded(
+                  flex: 40,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        FlatButton(
+                          color: Colors.black,
+                          textColor: Colors.white,
+                          disabledColor: Colors.grey,
+                          disabledTextColor: Colors.black,
+                          //padding: EdgeInsets.all(16.0),
 
-                      padding: EdgeInsets.only(
-                          left: 52.5, top: 15.0, right: 52.5, bottom: 15.0),
-                      splashColor: Colors.blueAccent,
-                      onPressed: () {
-                        print("Changing theater.");
-                      },
-                      child: Text(
-                        "CHANGE THEATER",
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ),
-                    Text(""),
-                    FlatButton(
-                      color: Colors.black,
-                      textColor: Colors.white,
-                      disabledColor: Colors.grey,
-                      disabledTextColor: Colors.black,
-                      padding: EdgeInsets.only(
-                          left: 50.0, top: 15.0, right: 50.0, bottom: 15.0),
-                      splashColor: Colors.blueAccent,
-                      onPressed: () {
-                        print("Search by preferences activated.");
-                      },
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            "EDIT YOUR TASTES",
+                          padding: EdgeInsets.only(
+                              left: 54.0, top: 25.0, right: 54.0, bottom: 25.0),
+                          splashColor: Colors.blueAccent,
+                          onPressed: () {
+                            print("New Search activated.");
+                          },
+                          child: Text(
+                            "NEW SEARCH",
                             style: TextStyle(fontSize: 20.0),
                           ),
-
-                        ],
-                      ),
+                        ),
+                        FlatButton(
+                          color: Colors.black,
+                          textColor: Colors.white,
+                          disabledColor: Colors.grey,
+                          disabledTextColor: Colors.black,
+                          padding: EdgeInsets.only(
+                              left: 50.0, top: 15.0, right: 50.0, bottom: 15.0),
+                          splashColor: Colors.blueAccent,
+                          onPressed: () {
+                            print("Search by preferences activated.");
+                          },
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "SEARCH BY",
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                              Text(
+                                "PREFERENCES",
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        //Text("Hello"),
+                        //Text("Hello"),
+                      ],
                     ),
-                    //Text("Hello"),
-                    //Text("Hello"),
-                  ],
+                  ),
                 ),
-              ),
+                Expanded(
+                  flex: 30,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        FlatButton(
+                          color: Colors.black,
+                          textColor: Colors.white,
+                          disabledColor: Colors.grey,
+                          disabledTextColor: Colors.black,
+                          //padding: EdgeInsets.all(16.0),
+
+                          padding: EdgeInsets.only(
+                              left: 52.5, top: 15.0, right: 52.5, bottom: 15.0),
+                          splashColor: Colors.blueAccent,
+                          onPressed: () {
+                            print("Changing theater.");
+                          },
+                          child: Text(
+                            "CHANGE THEATER",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        ),
+                        Text(""),
+                        FlatButton(
+                          color: Colors.black,
+                          textColor: Colors.white,
+                          disabledColor: Colors.grey,
+                          disabledTextColor: Colors.black,
+                          padding: EdgeInsets.only(
+                              left: 50.0, top: 15.0, right: 50.0, bottom: 15.0),
+                          splashColor: Colors.blueAccent,
+                          onPressed: () {
+                            print("Search by preferences activated.");
+                          },
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                "EDIT YOUR TASTES",
+                                style: TextStyle(fontSize: 20.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                        //Text("Hello"),
+                        //Text("Hello"),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
