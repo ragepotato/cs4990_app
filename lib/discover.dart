87f6fda0a9926f14ba991308c324ddb1dc9d5627
Stephen Tayag <stephen.taya@gmail.com>
2020-02-27 11:05:00 -0800

    Search page almost done

--- lib/discover.dart
+++ lib/discover.dart
@@ -41,9 +41,9 @@ class _MyDiscoverState extends State<MyDiscoverPage> {
     }
   }
 
+  String dropdownValues = 'G';
+  String filmLength = '< 90 mins';
   @override
-  String dropdownValue = "False";
-
 //  bool selectVal = false;
 //  bool selectVal2 = false;
 //  bool _selectVal2 = false;
@@ -51,7 +51,8 @@ class _MyDiscoverState extends State<MyDiscoverPage> {
 //  bool _secValue = false;
   List<bool> genreList = new List(8);
   int _selection;
-  double acclaimSlider = 0;
+  double acclaimSlider = 1;
+  double popularitySlider = 1;
   var num2;
 
   //create an array of booleans to determine what is being checked off
@@ -62,126 +63,270 @@ class _MyDiscoverState extends State<MyDiscoverPage> {
         // body: new Stack(
 
         body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Container(
-                  color: Colors.white,
-                  child: MultiSelectFormField(
-                    autovalidate: false,
-                    //titleText: 'My workouts',
-                    titleText: 'Genre',
-
-                    validator: (value) {
-                      if (value == null || value.length == 0) {
-                        return 'Please select one or more options';
-                      }
-                    },
-                    dataSource: [
-                      {
-                        "display": "Action",
-                        "value": "Action",
-                      },
-                      {
-                        "display": "Comedy",
-                        "value": "Comedy",
-                      },
-                      {
-                        "display": "Drama",
-                        "value": "Drama",
-                      },
-                      {
-                        "display": "Family",
-                        "value": "Family",
-                      },
-                      {
-                        "display": "Horror",
-                        "value": "Horror",
-                      },
-                      {
-                        "display": "Musical",
-                        "value": "Musical",
-                      },
-                      {
-                        "display": "Science Fiction",
-                        "value": "Science Fiction",
-                      },
-                    ],
-                    textField: 'display',
-                    valueField: 'value',
-                    okButtonLabel: 'OK',
-                    cancelButtonLabel: 'CANCEL',
-                    // required: true,
-                    hintText: 'Please choose one or more',
-                    value: _myActivities,
-                    onSaved: (value) {
-                      if (value == null) return;
-                      setState(() {
-                        _myActivities = value;
-                      });
-                    },
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.only(top: 36, bottom: 12),
+                    child: Text(
+                      'What to Watch?',
+                      style: TextStyle(
+                        fontSize: 30.0, // insert your font size here
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.all(16),
-                color: Colors.white,
-                child: Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: const Text(
-                        'Lafayette',
-                        style: TextStyle(
-                          fontSize: 15.0, // insert your font size here
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.all(9),
+                    child: Container(
+                      color: Colors.white,
+                      child: MultiSelectFormField(
+                        autovalidate: false,
+                        //titleText: 'My workouts',
+                        titleText: 'Genre',
+
+                        validator: (value) {
+                          if (value == null || value.length == 0) {
+                            return 'Please select one or more options';
+                          }
+                        },
+                        dataSource: [
+                          {
+                            "display": "Action",
+                            "value": "Action",
+                          },
+                          {
+                            "display": "Comedy",
+                            "value": "Comedy",
+                          },
+                          {
+                            "display": "Drama",
+                            "value": "Drama",
+                          },
+                          {
+                            "display": "Family",
+                            "value": "Family",
+                          },
+                          {
+                            "display": "Horror",
+                            "value": "Horror",
+                          },
+                          {
+                            "display": "Musical",
+                            "value": "Musical",
+                          },
+                          {
+                            "display": "Science Fiction",
+                            "value": "Science Fiction",
+                          },
+                        ],
+                        textField: 'display',
+                        valueField: 'value',
+                        okButtonLabel: 'OK',
+                        cancelButtonLabel: 'CANCEL',
+                        // required: true,
+                        hintText: 'Please choose one or more',
+                        value: _myActivities,
+                        onSaved: (value) {
+                          if (value == null) return;
+                          setState(() {
+                            _myActivities = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+
+//              Container(
+//                margin: EdgeInsets.all(16),
+//                color: Colors.white,
+//                child: Column(
+//                  children: <Widget>[
+//                    ListTile(
+//                      title: const Text(
+//                        'Lafayette',
+//                        style: TextStyle(
+//                          fontSize: 15.0, // insert your font size here
+//                        ),
+//                      ),
+//                      leading: Radio(
+//                        value: SingingCharacter.lafayette,
+//                        groupValue: _character,
+//                        onChanged: (SingingCharacter value) {
+//                          setState(() {
+//                            _character = value;
+//                          });
+//                        },
+//                      ),
+//                    ),
+//                    ListTile(
+//                      title: const Text('Thomas Jefferson'),
+//                      leading: Radio(
+//                        value: SingingCharacter.jefferson,
+//                        groupValue: _character,
+//                        onChanged: (SingingCharacter value) {
+//                          setState(() {
+//                            _character = value;
+//                          });
+//                        },
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Highest MPAA Rating? '),
+                        DropdownButton(
+                          value: dropdownValues,
+                          isDense: true,
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dropdownValues = newValue;
+                            });
+                          },
+                          items: ['G', 'PG', 'PG-13', 'R', 'NC-17']
+                              .map((String value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
                         ),
-                      ),
-                      leading: Radio(
-                        value: SingingCharacter.lafayette,
-                        groupValue: _character,
-                        onChanged: (SingingCharacter value) {
-                          setState(() {
-                            _character = value;
-                          });
-                        },
-                      ),
+                      ],
                     ),
-                    ListTile(
-                      title: const Text('Thomas Jefferson'),
-                      leading: Radio(
-                        value: SingingCharacter.jefferson,
-                        groupValue: _character,
-                        onChanged: (SingingCharacter value) {
-                          setState(() {
-                            _character = value;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Text("How much do you care about: "),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Acclaim?"),
-                  Slider(
-                    label: 'acclaim',
-                    value: acclaimSlider,
-                    onChanged: (double accAmount) {
-                      setState(() {
-                        acclaimSlider = accAmount;
-                      });
-                    },
                   ),
-                  Text(
-                      // double num2 = double.parse(num1.toStringAsFixed(2));
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Film Length? '),
+                        DropdownButton(
+                          value: filmLength,
+                          isDense: true,
+                          onChanged: (String anotherValue) {
+                            setState(() {
+                              filmLength = anotherValue;
+                            });
+                          },
+                          items: [
+                            '< 90 mins',
+                            '< 120 mins',
+                            '< 150 mins',
+                            '150 mins'
+                          ].map((String value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
 
-                      '$acclaimSlider' + '%')
-                ],
-              ),
+                Text("How much do you care abouta film's... "),
 
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Acclaim?"),
+                      Slider(
+                        //label: 'acclaim',
+                        value: acclaimSlider,
+                        onChanged: (double accAmount) {
+                          setState(() {
+                            acclaimSlider = accAmount;
+                          });
+                        },
+                        min: 1,
+                        max: 10,
+                        divisions: 9,
+                      ),
+                      Text(
+                          // double num2 = double.parse(num1.toStringAsFixed(2));
+
+                          '$acclaimSlider')
+                    ],
+                  ),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Popularity?"),
+                      Slider(
+                        //label: 'acclaim',
+                        value: popularitySlider,
+                        onChanged: (double popAmount) {
+                          setState(() {
+                            popularitySlider = popAmount;
+                          });
+                        },
+                        min: 1,
+                        max: 10,
+                        divisions: 9,
+                      ),
+                      Text(
+                          // double num2 = double.parse(num1.toStringAsFixed(2));
+
+                          '$popularitySlider')
+                    ],
+                  ),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.all(12),
+                    child: FlatButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.black,
+                      //padding: EdgeInsets.all(16.0),
+
+//                padding: EdgeInsets.only(
+//                    left: 52.5, top: 15.0, right: 52.5, bottom: 15.0),
+                      splashColor: Colors.blueAccent,
+                      onPressed: () {
+                        _saveForm();
+                        print('-----------------------------------');
+                        print(_myActivitiesResult);
+                        print(dropdownValues);
+                        print(filmLength);
+                        print("How much they care about acclaim: " +
+                            '$acclaimSlider');
+                        print("How much they care about popularity: " +
+                            '$popularitySlider');
+                      },
+                      child: Text(
+                        "SEARCH!",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ),
+                ),
 //              Container(
 //                padding: EdgeInsets.all(8),
 //                child: RaisedButton(
@@ -189,11 +334,12 @@ class _MyDiscoverState extends State<MyDiscoverPage> {
 //                  onPressed: _saveForm,
 //                ),
 //              ),
-//              Container(
-//                padding: EdgeInsets.all(16),
-//                child: Text(_myActivitiesResult),
-//              ),
-            ],
+//                Container(
+//                  padding: EdgeInsets.all(16),
+//                  child: Text(_myActivitiesResult),
+//                ),
+              ],
+            ),
           ),
         ));
     // This trailing comma makes auto-formatting nicer for build methods.
--- lib/pulled.dart
+++ lib/pulled.dart
@@ -31,86 +31,85 @@ class _MyHomePageState2 extends State<MyHomePage2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-//      Center(
-//        child:
-//        Form(
-//          key: formKey,
-//          child:
-
-          Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(16),
-            child: MultiSelectFormField(
-              autovalidate: false,
-              titleText: 'My workouts',
-              validator: (value) {
-                if (value == null || value.length == 0) {
-                  return 'Please select one or more options';
-                }
-              },
-              dataSource: [
-                {
-                  "display": "Writing",
-                  "value": "Running",
-                },
-                {
-                  "display": "Climbing",
-                  "value": "Climbing",
-                },
-                {
-                  "display": "Walking",
-                  "value": "Walking",
-                },
-                {
-                  "display": "Swimming",
-                  "value": "Swimming",
-                },
-                {
-                  "display": "Soccer Practice",
-                  "value": "Soccer Practice",
-                },
-                {
-                  "display": "Baseball Practice",
-                  "value": "Baseball Practice",
-                },
-                {
-                  "display": "Football Practice",
-                  "value": "Football Practice",
-                },
-              ],
-              textField: 'display',
-              valueField: 'value',
-              okButtonLabel: 'OK',
-              cancelButtonLabel: 'CANCEL',
-              // required: true,
-              hintText: 'Please choose one or more',
-              value: _myActivities,
-              onSaved: (value) {
-                if (value == null) return;
-                setState(() {
-                  _myActivities = value;
-                });
-              },
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(8),
-            child: RaisedButton(
-              child: Text('Save'),
-              onPressed: _saveForm,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(16),
-            child: Text(_myActivitiesResult),
-          )
-        ],
+      appBar: AppBar(
+        title: Text('MultiSelect Formfield Example'),
       ),
-//        ),
-//      ),
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16),
+                child: MultiSelectFormField(
+                  autovalidate: false,
+                  titleText: 'My workouts',
+                  validator: (value) {
+                    if (value == null || value.length == 0) {
+                      return 'Please select one or more options';
+                    }
+                  },
+                  dataSource: [
+                    {
+                      "display": "Running",
+                      "value": "Running",
+                    },
+                    {
+                      "display": "Climbing",
+                      "value": "Climbing",
+                    },
+                    {
+                      "display": "Walking",
+                      "value": "Walking",
+                    },
+                    {
+                      "display": "Swimming",
+                      "value": "Swimming",
+                    },
+                    {
+                      "display": "Soccer Practice",
+                      "value": "Soccer Practice",
+                    },
+                    {
+                      "display": "Baseball Practice",
+                      "value": "Baseball Practice",
+                    },
+                    {
+                      "display": "Football Practice",
+                      "value": "Football Practice",
+                    },
+                  ],
+                  textField: 'display',
+                  valueField: 'value',
+                  okButtonLabel: 'OK',
+                  cancelButtonLabel: 'CANCEL',
+                  // required: true,
+                  hintText: 'Please choose one or more',
+                  value: _myActivities,
+                  onSaved: (value) {
+                    if (value == null) return;
+                    setState(() {
+                      _myActivities = value;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: RaisedButton(
+                  child: Text('Save'),
+                  onPressed: _saveForm,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Text(_myActivitiesResult),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
